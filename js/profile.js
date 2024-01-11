71d28f327daa2e643afc8c88bd9b5908698c6fc1
Djibril SOW <djibrilsow1@esp.sn>
2024-01-11 00:38:31 +0000

    last changes

--- js/profile.js
+++ js/profile.js
@@ -72,33 +72,6 @@ function Profile(user, audits, chartData, barData){
     const xps = user.xp.aggregate.sum.amount
     const xp = Math.ceil(xps/1000);
 
-    const newHeader = `
-    <title>Talent Profile</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <!-- Favicon -->
-    <link href="img/favicon.ico" rel="icon">
-
-    <!-- Google Web Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-
-    <!-- Icon Font Stylesheet -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
-
-    <!-- Libraries Stylesheet -->
-    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
-    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
-    <link href="lib/animate/animate.min.css" rel="stylesheet">
-
-    <!-- Customized Bootstrap Stylesheet -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Template Stylesheet -->
-    <link href="css/style.css" rel="stylesheet">
-    `
-
     const newBody = `
     <met#aeb3b8et="utf-8">
     <div class="container-fluid">
@@ -241,7 +214,6 @@ function Profile(user, audits, chartData, barData){
     <script src="js/graphql.js"></script>
     `
 
-    document.head.innerHTML = newHeader;
     document.body.innerHTML = newBody;
 
     addAudits(audits)
